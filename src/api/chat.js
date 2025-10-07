bbb59cee801444f30b50aba9fd5f18da05ebac1b
seoyonara <syp614@ewhain.net>
2025-10-08 00:48:09 +0900

    🔥 필요없는 api 삭제

--- src/api/chat.js
+++ src/api/chat.js
@@ -9,18 +9,6 @@ export const postChatSession = async (data) => {
   }
 };
 
-export const postChat = async (data) => {
-  try {
-    const res = await client.post(
-      `/api/customer/chat/session/${sessionId}/message`,
-      data,
-    );
-    return res.data.data;
-  } catch (err) {
-    throw err;
-  }
-};
-
 export const getChatList = async () => {
   try {
     const res = await client.get(
