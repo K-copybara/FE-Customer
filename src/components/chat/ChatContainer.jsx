b2fd088333f5c5b2a987f67478938e3c11045a09
seoyonara <syp614@ewhain.net>
2025-10-20 01:54:16 +0900

    ✨ 장바구니 이동 버튼

--- src/components/chat/ChatContainer.jsx
+++ src/components/chat/ChatContainer.jsx
@@ -3,8 +3,25 @@ import SEMINI from '../../assets/semini.svg?react';
 import { UserMessage } from './UserMessage';
 import { AIMessage } from './AIMessage';
 import { LoadingChat } from './LoadingChat';
+import { useEffect, useState } from 'react';
+import { body_medium } from '../../styles/font';
+import { useNavigate } from 'react-router-dom';
 
 export const ChatContainer = ({ messageList, bottomRef, isLoading }) => {
+  const navigate = useNavigate();
+
+  const isCartAddSuccess = (list) => {
+    if (!Array.isArray(list) || list.length === 0) return false;
+    const last = list[list.length - 1];
+    return (
+      last.role === 'BOT' &&
+      typeof last.content === 'string' &&
+      last.content.trim().endsWith('장바구니에 성공적으로 추가되었습니다.')
+    );
+  };
+
+  const showCart = isCartAddSuccess(messageList);
+
   const groupMessages = (chatData) => {
     if (!chatData || chatData.length === 0) {
       return [];
@@ -58,6 +75,16 @@ export const ChatContainer = ({ messageList, bottomRef, isLoading }) => {
           </ChatGroup>
         </Wrapper>
       )}
+      {showCart && (
+        <Wrapper>
+          <AIcon />
+          <ChatGroup>
+            <CartText onClick={() => navigate('/cart')}>
+              장바구니 바로가기
+            </CartText>
+          </ChatGroup>
+        </Wrapper>
+      )}
 
       <div ref={bottomRef} />
     </Container>
@@ -96,4 +123,21 @@ const ChatGroup = styled.div`
 const AIcon = styled(SEMINI)`
   margin-top: auto;
   margin-bottom: 0.5rem;
+`;
+
+const CartText = styled.div`
+  display: flex;
+  width: 8rem;
+  justify-content: center;
+  align-items: center;
+
+  padding: 0.625rem;
+  border-radius: 0.625rem 0.625rem 0.625rem 0;
+  border: 1px solid var(--secondary);
+  background: var(--primary);
+  color: var(--white);
+  white-space: pre-line;
+  ${body_medium}
+
+  cursor: pointer;
 `;
