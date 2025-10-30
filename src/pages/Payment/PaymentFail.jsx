42b01aa08554b9ec5587bd8400bbf99d6d85cec0
seoyonara <syp614@ewhain.net>
2025-10-31 01:42:06 +0900

    🐛 페이지 높이 dvh로 수정

--- src/pages/Payment/PaymentFail.jsx
+++ src/pages/Payment/PaymentFail.jsx
@@ -74,7 +74,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  min-height: 100vh;
+  min-height: 100dvh;
   padding: 2rem;
   background: var(--background);
 `;
